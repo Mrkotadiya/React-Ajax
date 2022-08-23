9c6a7e63c472bb16d1b3f8e5b447267e378799f0
nirbhay <nirbhayk7281@gmail.com>
2022-08-23 17:26:54 +0530

    add lazy loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 // import axios from 'axios';
-import { Route, NavLink, Switch, Redirect } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
+// import NewPost from "./NewPost/NewPost";
+import asyncComponent from "../../hoc/asyncComponent";
 // import FullPost from "./FullPost/FullPost";
+
+const AsyncNewPost = asyncComponent(()=>{
+  return import('./NewPost/NewPost');
+  
+});
 
 class Blog extends Component {
   state = {
@@ -46,11 +52,11 @@ class Blog extends Component {
         <Switch>
           {/* <Route path="/new-post" component={NewPost} /> */}
           
-          {this.state.auth ?<Route path="/new-post" component={NewPost} /> : null}
-          <Route path="/posts/" component={Posts} /> 
-          <Route render={()=> <h1>Not Found</h1>} /> 
+          {this.state.auth ?<Route path="/new-post" component={AsyncNewPost} /> : null}
+          <Route path="/posts/" component={Posts}/> 
+          <Route render={()=> <h1>Not Found</h1>}/> 
           {/* <Route path="/posts/" component={Posts} /> */}
-          {/* <Redirect fro m="/" to="/posts" /> */}
+          {/* <Redirect from="/" to="/posts" /> */}
         </Switch>
       </div> 
     );
